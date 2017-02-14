123351734cd80b444abbc7f8812480df54dc5c1c
Jim Rohrer <jim.rohrer@gmail.com>
2017-02-14 14:17:07 -0800

    Check Mentor availability wip - http error 500

--- lib/kele.rb
+++ lib/kele.rb
@@ -10,8 +10,12 @@ class Kele
   end
 
   def get_me
-    response = self.class.get(@base_url + '/users/me', headers: { "authorization" => @auth_token })
+    response = self.class.get(@base_url + '/users/me', headers: { "authorization" => @auth_token } )
     p JSON.parse(response.body)
+  end
+
+  def get_mentor_availability(mentor_id)
+    response = self.class.get(@base_url + '/mentors/' + mentor_id.to_s + '/student_availability', { id: mentor_id, content_type: 'application/json', authorization: @auth_token } )
   end
 
 end

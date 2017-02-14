e84f68b9c40ee937e95813c2180610839284496c
Jim Rohrer <jim.rohrer@gmail.com>
2017-02-13 16:29:50 -0800

    Retrieve current user's JSON blob and convert to a Ruby hash

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,20 +1,17 @@
-require 'rubygems' if RUBY_VERSION < '1.9'
-require 'rest_client'
 require 'httparty'
 
 class Kele
   include HTTParty
-  attr_reader :user_token
 
   def initialize(email, password)
-
-    @email = email
-    @password = password
-    headers = {
-      :content_type => 'application/json'
-    }
-
     @base_url = 'https://www.bloc.io/api/v1'
-    @user_token = self.class.post('https://www.bloc.io/api/v1/sessions', { "email": @email, "password": @password }, headers )
+    reply = self.class.post(@base_url + '/sessions', body: { email: email, password: password } )
+    @auth_token = reply['auth_token']
   end
+
+  def get_me
+    response = self.class.get(@base_url + '/users/me', headers: { "authorization" => @auth_token })
+    p JSON.parse(response.body)
+  end
+
 end

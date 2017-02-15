85bf4927287b3660e934c182f1e40f13d729a9e7
Jim Rohrer <jim.rohrer@gmail.com>
2017-02-15 13:49:31 -0800

    Get mentor availability - HTTP ERROR 500

--- lib/kele.rb
+++ lib/kele.rb
@@ -10,12 +10,23 @@ class Kele
   end
 
   def get_me
-    response = self.class.get(@base_url + '/users/me', headers: { "authorization" => @auth_token } )
+    response = self.class.get("#{@base_url}/users/me", headers: { "authorization" => @auth_token } )
     p JSON.parse(response.body)
   end
 
   def get_mentor_availability(mentor_id)
-    response = self.class.get(@base_url + '/mentors/' + mentor_id.to_s + '/student_availability', { id: mentor_id, content_type: 'application/json', authorization: @auth_token } )
+    # Charlie's mentor_id is 529277
+    response = self.class.get("#{@base_url}/mentors/#{mentor_id.to_s}/student_availability", { id: mentor_id, content_type: 'application/json', authorization: @auth_token } )
   end
 
+  def get_roadmap(roadmap_id)
+    # My roadmap_id is 37
+    response = self.class.get("#{@base_url}/roadmaps/#{roadmap_id.to_s}", { id: 0, content_type: 'application/json', authorization: @auth_token } )
+    p JSON.parse(response.body)
+  end
+
+  def get_checkpoint(checkpoint_id)
+    # Use checkpoint_id of 2265 (Module 1: Frontend Programming Fundamentals)
+    response = self.class.get("#{@base_url}/checkpoints/#{checkpoint_id.to_s}", { content_type: 'application/json', authorization: @auth_token } )
+  end
 end

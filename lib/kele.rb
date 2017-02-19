24feabf34215b3c235e7e81ec2492745f3e03a84
Jim Rohrer <jim.rohrer@gmail.com>
2017-02-18 20:21:41 -0800

    Get messages and create a new message

--- lib/kele.rb
+++ lib/kele.rb
@@ -16,10 +16,40 @@ class Kele
     p JSON.parse(response.body)
   end
 
+  # def get_mentor_availability(mentor_id)
+  #   # Charlie's mentor_id is 529277
+  #   response = self.class.get("#{@base_url}/mentors/#{mentor_id.to_s}/student_availability", { id: mentor_id, content_type: 'application/json', authorization: @auth_token } )
+  #   p JSON.parse(response.body)
+  # end
+
   def get_mentor_availability(mentor_id)
     # Charlie's mentor_id is 529277
-    response = self.class.get("#{@base_url}/mentors/#{mentor_id.to_s}/student_availability", { id: mentor_id, content_type: 'application/json', authorization: @auth_token } )
+    response = self.class.get("#{@base_url}/mentors/#{mentor_id.to_s}/student_availability", headers: { authorization: @auth_token } )
     p JSON.parse(response.body)
+  end
+
+  def get_messages(msg_page = 0)
+    # Using undocumented technique of "?page=n" URL argument from github user hcbviolet from https://github.com/hcbviolet/kele
+    if msg_page > 0
+      append_uri = "?page=#{msg_page}"
+    else
+      append_uri = ""
+    end
+    response = self.class.get("#{@base_url}/message_threads#{append_uri}",  headers: { authorization: @auth_token } )
+    p JSON.parse(response.body)
+  end
+
+  def create_message(msg_subject, msg_text, thread_token = nil)
+    response = self.class.post("#{@base_url}/messages",
+        body: {
+          user_id: self.get_me['email'],
+          recipient_id: self.get_me['current_enrollment']['mentor_id'],
+#          token: thread_token,
+          subject: msg_subject,
+          'stripped-text' => msg_text
+          },
+        headers: { authorization: @auth_token } )
+      p JSON.parse(response.body)
   end
 
 end
--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -2,13 +2,13 @@ module Roadmap
 
   def get_roadmap(roadmap_id)
     # My roadmap_id is 37
-    response = self.class.get("#{@base_url}/roadmaps/#{roadmap_id.to_s}", { id: 0, content_type: 'application/json', authorization: @auth_token } )
+    response = self.class.get("#{@base_url}/roadmaps/#{roadmap_id.to_s}",  id: 0, headers: { content_type: 'application/json', authorization: @auth_token } )
     p JSON.parse(response.body)
   end
 
   def get_checkpoint(checkpoint_id)
-    # Use checkpoint_id of 2265 (Module 1: Frontend Programming Fundamentals)
-    response = self.class.get("#{@base_url}/checkpoints/#{checkpoint_id.to_s}", { content_type: 'application/json', authorization: @auth_token } )
+    # Use checkpoint_id of 2265 for testing (Module 1: Frontend Programming Fundamentals)
+    response = self.class.get("#{@base_url}/checkpoints/#{checkpoint_id.to_s}", headers: { content_type: 'application/json', authorization: @auth_token } )
     p JSON.parse(response.body)
   end
 
